869156e46c34ff4f12a15bee0a1281352f820aa0
dongfusong <dongfusong@126.com>
2019-08-07 22:15:32 +0800

    xxx

--- src/page/mainPage/Cooperators.vue
+++ src/page/mainPage/Cooperators.vue
@@ -1,170 +1,160 @@
 <template>
   <div class="view column-box-center cover">
-    <div class="bg-title-font title-margin-top">
-      合作单位
-    </div>
-    <div class="sm-title-font content-margin-top">
-      北京—杭州—厦门，营造中国与世界的沟通之所 建筑语言：功能流线，空间结构，形态外延，文化内涵
-    </div>
+    <div class="bg-title-font title-margin-top">合作单位</div>
+    <div class="sm-title-font content-margin-top">北京—杭州—厦门，营造中国与世界的沟通之所 建筑语言：功能流线，空间结构，形态外延，文化内涵</div>
 
-    <div style="width: 100%;" class=" title-margin-top">
+    <div style="width: 100%;" class="title-margin-top">
       <el-carousel :interval="5000" arrow="always" height="240px" ref="carousel">
         <el-carousel-item>
           <div class="row-box-center">
             <div class="pic-size pic-right-margin">
-              <img src="/static/main/金砖.png" alt="" height="100%" width="100%">
+              <img src="/static/main/金砖.png" alt height="100%" width="100%" />
             </div>
             <div class="pic-size pic-right-margin">
-              <img src="/static/main/G20.png" alt="" height="100%" width="100%">
+              <img src="/static/main/G20.png" alt height="100%" width="100%" />
             </div>
             <div class="pic-size pic-right-margin">
-              <img src="/static/main/一带一路.png" alt="" height="100%" width="100%">
+              <img src="/static/main/一带一路.png" alt height="100%" width="100%" />
             </div>
             <div class="pic-size pic-right-margin">
-              <img src="/static/main/apec.png" alt="" height="100%" width="100%">
+              <img src="/static/main/apec.png" alt height="100%" width="100%" />
             </div>
           </div>
-          </el-carousel-item>
-           <el-carousel-item>
-            <div class="row-box-center">
-              <div class="pic-size pic-right-margin">
-                <img src="/static/main/金砖.png" alt="" height="100%" width="100%">
-              </div>
-              <div class="pic-size pic-right-margin">
-                <img src="/static/main/G20.png" alt="" height="100%" width="100%">
-              </div>
-              <div class="pic-size pic-right-margin">
-                <img src="/static/main/一带一路.png" alt="" height="100%" width="100%">
-              </div>
-              <div class="pic-size pic-right-margin">
-                <img src="/static/main/apec.png" alt="" height="100%" width="100%">
-              </div>
+        </el-carousel-item>
+        <el-carousel-item>
+          <div class="row-box-center">
+            <div class="pic-size pic-right-margin">
+              <img src="/static/main/金砖.png" alt height="100%" width="100%" />
             </div>
-            </el-carousel-item>
+            <div class="pic-size pic-right-margin">
+              <img src="/static/main/G20.png" alt height="100%" width="100%" />
+            </div>
+            <div class="pic-size pic-right-margin">
+              <img src="/static/main/一带一路.png" alt height="100%" width="100%" />
+            </div>
+            <div class="pic-size pic-right-margin">
+              <img src="/static/main/apec.png" alt height="100%" width="100%" />
+            </div>
+          </div>
+        </el-carousel-item>
       </el-carousel>
     </div>
 
-
     <div class="row-box-center box-right-offset" style="margin-top: 162px; margin-bottom: 79px;">
-
       <div class="item-right-margin column-box-center">
         <div class="sm-pic-size sm-pic-bottom">
-          <img src="/static/main/编组 1.png" alt="" height="100%" width="100%">
+          <img src="/static/main/编组 1.png" alt height="100%" width="100%" />
         </div>
-        <div class="sm-content-font">
-          010 - 87213595
-        </div>
+        <div class="sm-content-font">010 - 87213595</div>
       </div>
 
       <div class="item-right-margin column-box-center">
         <div class="sm-pic-size sm-pic-bottom">
-          <img src="/static/main/编组 2.png" alt="" height="100%" width="100%">
+          <img src="/static/main/编组 2.png" alt height="100%" width="100%" />
         </div>
-        <div class="sm-content-font">
-          23429486@qq.com
-        </div>
+        <div class="sm-content-font">23429486@qq.com</div>
       </div>
       <div class="column-box-center">
         <div class="sm-pic-size sm-pic-bottom">
-          <img src="/static/main/编组 3.png" alt="" height="100%" width="100%">
+          <img src="/static/main/编组 3.png" alt height="100%" width="100%" />
         </div>
-        <div class="sm-content-font">
-          北京市西城区百万庄大街11号
-        </div>
+        <div class="sm-content-font">北京市西城区百万庄大街11号</div>
       </div>
     </div>
 
-    <div class="sm-content-font" style="margin-bottom: 61px;">
-      Copyright@ 2008-2020 BlueFocus. All Rights Reserved 京ICP备19003641
-    </div>
+    <div
+      class="sm-content-font"
+      style="margin-bottom: 61px;"
+    >Copyright@ 2008-2020 BlueFocus. All Rights Reserved 京ICP备19003641</div>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'Cooperators'
-  }
+export default {
+  name: "Cooperators"
+};
 </script>
 
 <style lang="less" scoped>
-  @import '../appStyle';
+@import "../appStyle";
 
-  .box-right-offset {
-    margin-left: 100px;
-  }
+.box-right-offset {
+  margin-left: 100px;
+}
 
-  .title-margin-top {
-    margin-top: 80px;
-  }
+.title-margin-top {
+  margin-top: 80px;
+}
 
-  .content-margin-top {
-    margin-top: 36px;
-  }
+.content-margin-top {
+  margin-top: 36px;
+}
 
-  .item-right-margin {
-    margin-right: 142px;
-  }
+.item-right-margin {
+  margin-right: 142px;
+}
 
-  .sm-pic-bottom {
-    margin-bottom: 20px;
-  }
+.sm-pic-bottom {
+  margin-bottom: 20px;
+}
 
-  .sm-pic-size {
-    width: 66px;
-    height: 66px;
-  }
+.sm-pic-size {
+  width: 66px;
+  height: 66px;
+}
 
-  .bg-title-font {
-    font-size: 30px;
-    font-family: PingFangSC-Regular;
-    font-weight: 400;
-    color: rgba(51, 51, 51, 1);
-    line-height: 42px;
-  }
+.bg-title-font {
+  font-size: 30px;
+  font-family: PingFangSC-Regular;
+  font-weight: 400;
+  color: rgba(51, 51, 51, 1);
+  line-height: 42px;
+}
 
-  .sm-title-font {
-    font-size: 14px;
-    font-family: PingFangSC-Regular;
-    font-weight: 400;
-    color: rgba(51, 51, 51, 1);
-    line-height: 22px;
-  }
+.sm-title-font {
+  font-size: 14px;
+  font-family: PingFangSC-Regular;
+  font-weight: 400;
+  color: rgba(51, 51, 51, 1);
+  line-height: 22px;
+}
 
-  .sm-content-font {
-    font-size: 16px;
-    font-family: Helvetica;
-    color: rgba(102, 102, 102, 1);
-    line-height: 19px;
-  }
+.sm-content-font {
+  font-size: 16px;
+  font-family: Helvetica;
+  color: rgba(102, 102, 102, 1);
+  line-height: 19px;
+}
 
+.pic-right-margin {
+  margin-right: 13px;
+}
 
+.column-box-center {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
 
-  .pic-right-margin {
-    margin-right: 13px;
-  }
+.pic-size {
+  width: 289px;
+  height: 230px;
+  // background-color: aliceblue;
+  border: 1px solid gainsboro;
+}
 
-  .column-box-center {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
+.pic-size:hover{
+    box-shadow: 2px 2px 5px #333333;
+}
 
-  .pic-size {
-    width: 289px;
-    height: 230px;
-    // background-color: aliceblue;
-    border:1px solid gainsboro;
-  }
+.row-box-center {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+}
 
-  .row-box-center {
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-  }
-
-  .view {
-    width: 100%;
-    // height: 618px;
-
-  }
+.view {
+  width: 100%;
+  // height: 618px;
+}
 </style>
