3dd7e97db3c06e3f9ae488283b95a2a39f31e519
dongfusong <dongfusong@126.com>
2019-09-10 00:04:43 +0800

    commit

--- config/index.js
+++ config/index.js
@@ -8,16 +8,16 @@ module.exports = {
   dev: {
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
-    proxyTable: {
-      '/liufanglei_server': {
-        target: 'http://localhost:9931',
-        changeOrigin: true,
-        pathRewrite: {
-          '^/liufanglei_server': 'liufanglei_server'
-        }
-      }
-    },
+    assetsPublicPath: './',
+    // proxyTable: {
+    //   '/liufanglei_server': {
+    //     target: 'http://localhost:9931',
+    //     changeOrigin: true,
+    //     pathRewrite: {
+    //       '^/liufanglei_server': 'liufanglei_server'
+    //     }
+    //   }
+    // },
 
     // Various Dev Server  settings
     host: 'localhost', // can be overwritten by process.env.HOST
--- src/page/civilInfo/index.vue
+++ src/page/civilInfo/index.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="history-contaienr">
-    <div class="backgroundImg">
-      <img src="/static/civil/故宫-午门.png" alt height="100%" width="100%" />
-    </div>
+  <div class="history-container">
     <div style="position: absolute; top:0; width:100%">
       <AppHeader noBackground="true" />
+    </div>
+    <div class="backgroundImg">
+      <img src="/static/civil/故宫-午门.png" alt height="100%" width="100%" />
     </div>
 
     <div class="location-view row-space">
@@ -18,8 +18,7 @@
         <div style="margin-top: 32px; width: 655px; height: 120px;" class="sm-title">
           作为15至20世纪明清皇帝的皇室住所，北京和沉阳的明清皇宫是晚期封建中国的国家权力中心。明朝皇帝朱棣在1406年至1420年间建造了明清故宫，
           由明朝皇帝朱棣于1406年至1420年建造，见证了明朝和明朝十五位皇帝在接下来的505年中的登基。沉阳故宫建于1625年至1637年间由Nurgaci为
-          清代女真/满族祖先建造，
-          于1644年在北京建立。也被称为后津宫或盛林宫，后来被用作王室的二级资本和临时住所，直到1911年。
+          清代女真/满族祖先建造，于1644年在北京建立。也被称为后津宫或盛林宫，后来被用作王室的二级资本和临时住所，直到1911年。
         </div>
       </div>
       <div style="width: 218px; height: 218px; margin-left: 203px;">
@@ -31,6 +30,11 @@
         历史文明
       </div>
       <ScrollTimeView></ScrollTimeView>
+    </div>
+    <div class="view-title share-pos" >
+        <div style="margin-left: 86px;">
+          用户分享
+        </div>
     </div>
   </div>
 </template>
@@ -56,6 +60,13 @@
 <style lang="less" scoped>
   // @import '../appStyle.less';
 
+  .share-pos{
+    margin-top: 180px;
+    width: 100%;
+    height: 800px;
+    box-sizing: border-box;
+    background-color: aliceblue;
+  }
   .backgroundImg{
     width: 100%;
     height: 800px;
@@ -106,7 +117,7 @@
     line-height: 24px;
   }
 
-  .history-contaienr {
+  .history-container {
     color: white;
     width: 100%;
     height: 100%;
